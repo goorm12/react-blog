c233613a11fcbdb2f2e37a84ef54f47d31c98710
binwoonki <vinwoonki98@gmail.com>
2024-09-24 21:59:35 +0900

    PostFeed 컴포넌트 UI 완성

--- src/component/Main.jsx
+++
@@ -1,7 +0,0 @@
-import "./Main.css";
-
-const Main = ({ title }) => {
-  return <main className="Main">{title}</main>;
-};
-
-export default Main;
---
+++ src/component/PostFeed.jsx
@@ -0,0 +1,30 @@
+import "./PostFeed.css";
+const PostFeed = () => {
+  return (
+    <div className="PostFeed">
+      <div className="postFeed-img">
+        <img src="/src/assets/js.png" alt="javascript" />
+      </div>
+
+      <div className="postFeed-content">
+        <div className="user-info">
+          <div className="user"></div>
+          <span>유저 네임</span>
+          <span>날짜</span>
+        </div>
+        <span>블로그 제목</span>
+
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam
+        </span>
+
+        <div className="postFeed-button">
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostFeed;
