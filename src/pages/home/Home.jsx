d31a0e59599f6e2c58ae432a4915236a4042f583
binwoonki <vinwoonki98@gmail.com>
2024-09-24 21:14:49 +0900

    Home 페이지

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,15 @@
+import Header from "../../component/Header";
+import Footer from "../../component/Footer";
+import Main from "../../component/Main";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
