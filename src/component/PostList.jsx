a37d856157a19124dfd6826297d75567f39d8976
binwoonki <vinwoonki98@gmail.com>
2024-09-24 21:59:52 +0900

    PostList 컴포넌트 Test

---
+++ src/component/PostList.jsx
@@ -0,0 +1,16 @@
+import PostFeed from "./PostFeed";
+import "./PostList.css";
+
+const PostList = () => {
+  return (
+    <main className="PostList">
+      <PostFeed />
+      <PostFeed />
+      <PostFeed />
+      <PostFeed />
+      <PostFeed />
+    </main>
+  );
+};
+
+export default PostList;
