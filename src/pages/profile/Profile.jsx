be61a535248f3fe85e2ee5e8e4340ab7583a5d5c
binwoonki <vinwoonki98@gmail.com>
2024-09-24 21:15:13 +0900

    Profile 페이지

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,15 @@
+import Footer from "../../component/Footer";
+import Header from "../../component/Header";
+import Main from "../../component/Main";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <Header />
+      <Main title={"Profile"} />
+      <Footer />
+    </div>
+  );
 };
 
 export default Profile;
