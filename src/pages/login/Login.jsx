7276dc90dd10c487462055a880c5be7cfc35dfe9
binwoonki <vinwoonki98@gmail.com>
2024-09-24 21:14:57 +0900

    Login 페이지

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,15 @@
+import Footer from "../../component/Footer";
+import Header from "../../component/Header";
+import Main from "../../component/Main";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <Main title={"Login"} />
+      <Footer />
+    </div>
+  );
 };
 
 export default Login;
