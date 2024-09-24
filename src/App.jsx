aee2aca6202eba589222135d17c7fcb6066e05dc
binwoonki <vinwoonki98@gmail.com>
2024-09-24 20:41:04 +0900

    라우팅 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Posts from "./pages/posts/Posts";
+import Profile from "./pages/profile/Profile";
+import SignUp from "./pages/signup/Signup";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
