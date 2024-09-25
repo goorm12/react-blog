2d783dafae696ce672a661d53ecb88ade6720253
binwoonki <vinwoonki98@gmail.com>
2024-09-25 16:39:29 +0900

    로그인 기능 작업
    firebase를 이용하여 작업 완료

--- src/component/LoginInput.jsx
+++ src/component/LoginInput.jsx
@@ -1,9 +1,36 @@
 import { Link } from "react-router-dom";
 import "./LoginInput.css";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
 const LoginInput = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const nav = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      nav("/", { replace: true });
+      console.log(userCredential.user);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <section className="LoginInput">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleLogin}>
         <div className="email-container">
           <label htmlFor="email">이메일</label>
           <input
@@ -11,6 +38,10 @@ const LoginInput = () => {
             id="email"
             name="email"
             placeholder="이메일을 입력해주세요"
+            autoComplete="off"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
         </div>
 
@@ -21,6 +52,10 @@ const LoginInput = () => {
             id="password"
             name="password"
             placeholder="비밀번호를 입력해주세요"
+            autoComplete="off"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </div>
 
@@ -29,6 +64,8 @@ const LoginInput = () => {
           <span className="signup-text">계정이 없으신가요?</span>
           <Link to={"/signup"}>회원가입</Link>
         </div>
+
+        {error && <span>{error}</span>}
       </form>
     </section>
   );
