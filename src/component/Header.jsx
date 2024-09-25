f692f6302375071333ffe5d27bfe95814d544872
binwoonki <vinwoonki98@gmail.com>
2024-09-25 17:11:57 +0900

    Header 스타일 수정(높이 및 패딩값), props로 동적으로 변경하기

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 
-const Header = () => {
+const Header = ({ rightChild }) => {
   return (
     <header className="Header">
       <h1 className="header-title">
         <Link to={"/"}>GoormLog</Link>
       </h1>
+
+      <div className="span-container">{rightChild}</div>
     </header>
   );
 };
