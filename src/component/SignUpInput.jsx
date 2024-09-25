1bcebcaeb02570e3bec7f45e788bbd99bb36529a
binwoonki <vinwoonki98@gmail.com>
2024-09-25 16:31:14 +0900

    회원가입 firebase를 이용하여 기능 구현하기, 패스워드 유효성검사, 이메일, 비밀번호 일치 일시 회원가입 버튼 활성화 회원가입 완료 후 home 페이지로 이동  home 페이지로 이동후 뒤로가기 x

--- src/component/SignUpInput.jsx
+++ src/component/SignUpInput.jsx
@@ -1,17 +1,66 @@
 import { Link } from "react-router-dom";
 import "./SignUpInput.css";
+import { useEffect, useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 const SignUpInput = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(true);
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState("");
+
+  const nav = useNavigate();
+  const passwordRegex = /^\d{8,}$/;
+
+  useEffect(() => {
+    if (!email && !password && !confirmPassword) {
+      setIsSubmitting(true);
+    } else if (
+      email &&
+      passwordRegex.test(password) &&
+      password === confirmPassword
+    ) {
+      setIsSubmitting(false);
+    }
+  }, [email, password, confirmPassword]);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      nav("/", { replace: true });
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
     <section className="SignUpInput">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSignUp}>
         <div className="email-container">
           <label htmlFor="email-input">이메일</label>
           <input
-            type="text"
+            type="email"
             name="email-input"
             id="email-input"
             placeholder="이메일을 입력하세요"
+            autoComplete="new-username"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
           />
         </div>
 
@@ -22,14 +71,42 @@ const SignUpInput = () => {
             id="password-input"
             name="password-input"
             placeholder="비밀번호를 입력하세요"
+            autoComplete="new-password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+              if (!passwordRegex.test(password)) {
+                setErrorPassword(
+                  "비밀번호는 8자리 이상 숫자만 포함해야 합니다."
+                );
+              } else {
+                setErrorPassword("");
+              }
+            }}
+            required
           />
+          {errorPassword && (
+            <span className="errorMessage">{errorPassword}</span>
+          )}
           <label htmlFor="password-input-2">비밀번호 확인</label>
           <input
             type="password"
             id="password-input-2"
             name="password-input-2"
             placeholder="비밀번호를 다시 입력하세요"
+            autoComplete="new-password"
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+              if (e.target.value === password) {
+                setErrorConfirmPassword("");
+              } else {
+                setErrorConfirmPassword("비밀번호가 일치하지 않습니다.");
+              }
+            }}
+            required
           />
+          {errorConfirmPassword && (
+            <span className="errorMessage">{errorConfirmPassword}</span>
+          )}
         </div>
 
         <div className="login-container">
@@ -37,7 +114,10 @@ const SignUpInput = () => {
           <Link to={"/login"}>로그인 하러가기</Link>
         </div>
 
-        <button className="submit-button">회원가입하기</button>
+        {error && <span className="errorMessage">{error}</span>}
+        <button className="submit-button" disabled={isSubmitting}>
+          회원가입하기
+        </button>
       </form>
     </section>
   );
