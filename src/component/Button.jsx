97177527cc8f9e2be4a5bfba183a1e2c26ea8a03
binwoonki <vinwoonki98@gmail.com>
2024-09-25 20:33:25 +0900

    다크모드 수정

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,8 +1,14 @@
+import { useTheme } from "../context/ThemeProvider";
 import "./Button.css";
 const Button = ({ onClick, text }) => {
+  const { theme } = useTheme();
   return (
     <>
-      <button onClick={onClick} className="button">
+      <button
+        onClick={onClick}
+        className="button"
+        style={{ color: theme === "dark" ? "#fff" : "#000" }}
+      >
         {text}
       </button>
     </>
