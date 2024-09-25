10c6e95836da7e53dd2c30fd140a38b4074a6c39
binwoonki <vinwoonki98@gmail.com>
2024-09-25 20:32:56 +0900

    다크모드 호환위해 수정

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
 
 import "./Header.css";
+import { useTheme } from "../context/ThemeProvider";
 
 const Header = ({ rightChild, swiperChild }) => {
+  const { theme } = useTheme();
   return (
-    <header className="Header-container">
+    <header
+      className="Header-container"
+      style={{
+        backgroundColor: theme === "dark" ? "#333" : "#fff",
+        color: theme === "dark" ? "#fff" : "#000",
+      }}
+    >
       <nav className="nav">
         <h1 className="header-title">
-          <Link to={"/"}>GoormLog</Link>
+          <Link
+            to={"/"}
+            style={{ color: theme === "dark" ? "#fff" : "#87ceeb" }}
+          >
+            GoormLog
+          </Link>
         </h1>
         <div className="span-container">{rightChild}</div>
       </nav>
