ba4b5e5d657163571b28da51f8a04dacd68aff93
binwoonki <vinwoonki98@gmail.com>
2024-09-25 17:12:30 +0900

    Home 페이지 user 로그인 및 그에 맞는 로그아웃 생성

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,62 @@
 import Header from "../../component/Header";
 import Footer from "../../component/Footer";
 import PostList from "../../component/PostList";
+import { Link } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import { useState, useEffect } from "react";
+import Button from "../../component/Button";
 
 const Home = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
   return (
     <div className="Home">
-      <Header />
+      <Header
+        rightChild={
+          user ? (
+            <>
+              <span>{user.email.split("@")[0]}님, 환영합니다!</span>
+              <Button onClick={handleLogout} text={"로그아웃"} />
+            </>
+          ) : (
+            <>
+              <Link to={"/login"}>로그인</Link>
+              <Link to={"/signup"}>회원가입</Link>
+              <Link to={"/posts"}>글쓰기</Link>
+            </>
+          )
+        }
+      />
       <PostList />
-      <Footer />
+      <Footer
+        child={
+          user ? (
+            <>
+              <Button onClick={handleLogout} text={"로그아웃"} />
+            </>
+          ) : (
+            <>
+              <Link to={"/login"}>로그인하기</Link>
+            </>
+          )
+        }
+      />
     </div>
   );
 };
