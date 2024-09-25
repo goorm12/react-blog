ab30a4f74116f7df9a6fd990d0f299d746ba9e3a
binwoonki <vinwoonki98@gmail.com>
2024-09-25 18:44:07 +0900

    Header 컴포넌트에 swiper 기능 추가

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,56 @@
 import { Link } from "react-router-dom";
+
 import "./Header.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 const Header = ({ rightChild }) => {
   return (
-    <header className="Header">
-      <h1 className="header-title">
-        <Link to={"/"}>GoormLog</Link>
-      </h1>
-
-      <div className="span-container">{rightChild}</div>
-    </header>
+    <div className="Header-container">
+      <nav className="nav">
+        <h1 className="header-title">
+          <Link to={"/"}>GoormLog</Link>
+        </h1>
+        <div className="span-container">{rightChild}</div>
+      </nav>
+      <header className="header">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={0}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          loop={true}
+        >
+          <SwiperSlide>
+            <img
+              src="/assets/bgc1.jpg"
+              alt=""
+              style={{ width: "100%", height: "auto" }}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="/assets/bgc2.jpg"
+              alt=""
+              style={{ width: "100%", height: "auto" }}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="/assets/bgc3.jpg"
+              alt=""
+              style={{ width: "100%", height: "auto" }}
+            />
+          </SwiperSlide>
+        </Swiper>
+      </header>
+    </div>
   );
 };
 
