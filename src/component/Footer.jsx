4ad746e54f5a7e17486242af68a312f9f7ae0f72
binwoonki <vinwoonki98@gmail.com>
2024-09-25 20:33:05 +0900

    다크 모드 적용

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,17 +1,32 @@
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
+import Button from "./Button";
+import { useTheme } from "../context/ThemeProvider";
+
 const Footer = ({ child }) => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <footer className="Footer">
+    <footer
+      className="Footer"
+      style={{
+        backgroundColor: theme === "dark" ? "#333" : "#fff",
+        color: theme === "dark" ? "#fff" : "#000",
+      }}
+    >
       <nav>
         <ul className="footer-ul">
           <li>
-            <Link to={"/posts"}>글 쓰기</Link>
+            <Link
+              to={"/posts"}
+              style={{ color: theme === "dark" ? "#fff" : "#000" }}
+            >
+              글 쓰기
+            </Link>
           </li>
           <li>{child}</li>
           <li>
-            <button>dark mode</button>
+            <Button text={"Dark"} onClick={toggleTheme} />
           </li>
         </ul>
       </nav>
