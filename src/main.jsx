ffa0992c507cd09c50768afa3dffa4da691e03a8
binwoonki <vinwoonki98@gmail.com>
2024-09-25 20:33:19 +0900

    Theme context 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { ThemeProvider } from "./context/ThemeProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </AuthProvider>
   </BrowserRouter>
 );
