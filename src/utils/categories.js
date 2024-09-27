f7524a8cc36d7e9c0c4439e723acb55b2a2541c0
binwoonki <vinwoonki98@gmail.com>
2024-09-27 16:29:03 +0900

    카테고리를 따로 분류해서 어느곳에서든 사용할 수 있게 함

---
+++ src/utils/categories.js
@@ -0,0 +1,8 @@
+export const categories = [
+  { id: 0, title: "" },
+  { id: 1, title: "All" },
+  { id: 2, title: "Web" },
+  { id: 3, title: "FrontEnd" },
+  { id: 4, title: "React" },
+  { id: 5, title: "Next.js" },
+];
