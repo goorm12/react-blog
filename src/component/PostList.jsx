d7c8541c8446d5bb323dcf4adffd610c2fc1c356
binwoonki <vinwoonki98@gmail.com>
2024-09-27 16:30:40 +0900

    코드 중복을 줄이고 카테고리 구현

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
 import { useTheme } from "../context/ThemeProvider";
 import PostFeed from "./PostFeed";
 import "./PostList.css";
+import { categories } from "../utils/categories";
 
 const PostList = () => {
   const { theme } = useTheme("light");
+  const [categoryFilter, setCategoryFilter] = useState("All");
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const handleCategory = (category) => {
+    setCategoryFilter(category);
+    setActiveCategory(category);
+  };
+
+  const filteredEmptyCategory = categories.filter(
+    (category) => category.title !== ""
+  );
+
   return (
     <main
       className="PostList"
       style={{ backgroundColor: theme === "dark" ? "#333" : "#fff" }}
     >
-      <div>
-        <PostFeed />
+      <div className="postlist-container">
+        <div className="categories-container">
+          <ul>
+            {filteredEmptyCategory.map((category) => {
+              return (
+                <li key={category.id}>
+                  <button
+                    className={
+                      activeCategory === category.title ? "active" : ""
+                    }
+                    onClick={() => {
+                      handleCategory(category.title);
+                    }}
+                  >
+                    {category.title}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="feed-container">
+          <PostFeed categoryFilter={categoryFilter} />
+        </div>
       </div>
     </main>
   );
